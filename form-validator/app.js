f0213e5b5092061539e15636ab118ad1914fdfbe
Andrew Rowley <andrew@internetdrew.com>
2022-03-09 14:11:31 -0500

    Add password matching funtionality

--- form-validator/app.js
+++ form-validator/app.js
@@ -19,37 +19,69 @@ function showSuccess(input) {
 }
 
 // Check email validity
-function isValidEmail(email) {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    )
-    ? true
-    : false;
+function checkEmail(input) {
+  if (input.value !== '') {
+    String(input.value)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+      ? showSuccess(input)
+      : showError(input, 'Email is invalid');
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters.`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} should be less than ${max} characters.`
+    );
+  }
+}
+
+// Check password match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== '' && input2.value !== '') {
+    if (input1.value !== input2.value) {
+      showError(input1, 'Passwords do not match');
+      showError(input2, 'Passwords do not match');
+    } else {
+      showSuccess(input1);
+      showSuccess(input2);
+    }
+  }
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Event listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  username.value === ''
-    ? showError(username, 'Username is required')
-    : showSuccess(username);
-
-  if (email.value === '') {
-    showError(email, 'Email is required');
-  } else if (!isValidEmail(email.value)) {
-    showError(email, 'Email is invalid');
-  } else {
-    showSuccess(email);
-  }
-
-  password.value === ''
-    ? showError(password, 'Password is required')
-    : showSuccess(password);
-
-  password2.value === ''
-    ? showError(password2, 'Password 2 is required')
-    : showSuccess(password2);
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });

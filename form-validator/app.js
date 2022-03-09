3e58c055c8d2d65e0ebf5b36b2678073eb67f995
Andrew Rowley <andrew@internetdrew.com>
2022-03-09 14:36:54 -0500

    Improve function names

--- form-validator/app.js
+++ form-validator/app.js
@@ -4,84 +4,76 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-// Show input error message
-function showError(input, message) {
-  const formControl = input.parentElement;
+function showErrorStatus(inputField, message) {
+  const formControl = inputField.parentElement;
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
   small.innerText = message;
 }
 
-// Show suucess outline
-function showSuccess(input) {
-  const formControl = input.parentElement;
+function showSuccessStatus(inputField) {
+  const formControl = inputField.parentElement;
   formControl.className = 'form-control success';
 }
 
-// Check email validity
-function checkEmail(input) {
-  if (input.value !== '') {
-    String(input.value)
+function checkEmailValidity(inputField) {
+  if (inputField.value !== '') {
+    String(inputField.value)
       .toLowerCase()
       .match(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       )
-      ? showSuccess(input)
-      : showError(input, 'Email is invalid');
+      ? showSuccessStatus(inputField)
+      : showErrorStatus(inputField, 'Email is invalid');
   }
 }
 
-// Check required fields
-function checkRequired(inputArr) {
-  inputArr.forEach((input) => {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
+function checkRequired(inputFieldArr) {
+  inputFieldArr.forEach((inputField) => {
+    if (inputField.value.trim() === '') {
+      showErrorStatus(inputField, `${getFieldName(inputField)} is required`);
     } else {
-      showSuccess(input);
+      showSuccessStatus(inputField);
     }
   });
 }
 
-// Check input length
-function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters.`
+function checkInputLength(inputField, min, max) {
+  if (inputField.value.length < min) {
+    showErrorStatus(
+      inputField,
+      `${getFieldName(inputField)} must be at least ${min} characters.`
     );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} should be less than ${max} characters.`
+  } else if (inputField.value.length > max) {
+    showErrorStatus(
+      inputField,
+      `${getFieldName(inputField)} should be less than ${max} characters.`
     );
   }
 }
 
-// Check password match
-function checkPasswordsMatch(input1, input2) {
-  if (input1.value !== '' && input2.value !== '') {
-    if (input1.value !== input2.value) {
-      showError(input1, 'Passwords do not match');
-      showError(input2, 'Passwords do not match');
+function checkPasswordsMatch(inputField1, inputField2) {
+  if (inputField1.value !== '' && inputField2.value !== '') {
+    if (inputField1.value !== inputField2.value) {
+      showErrorStatus(inputField1, 'Passwords do not match');
+      showErrorStatus(inputField2, 'Passwords do not match');
     } else {
-      showSuccess(input1);
-      showSuccess(input2);
+      showSuccessStatus(inputField1);
+      showSuccessStatus(inputField2);
     }
   }
 }
 
-// Get fieldname
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+function getFieldName(inputField) {
+  return inputField.id.charAt(0).toUpperCase() + inputField.id.slice(1);
 }
 
-// Event listeners
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
-  checkLength(username, 3, 15);
-  checkLength(password, 6, 25);
-  checkEmail(email);
+  checkInputLength(username, 3, 15);
+  checkInputLength(password, 6, 25);
+  checkEmailValidity(email);
   checkPasswordsMatch(password, password2);
 });

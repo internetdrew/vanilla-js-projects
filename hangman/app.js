4cc42ee1fbc3ec0f606d1c035e2c08ad900d4469
Andrew Rowley <andrew@internetdrew.com>
2022-08-16 12:18:11 -0400

    add functionality to show current word

--- hangman/app.js
+++ hangman/app.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-btn');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show the hidden word
+const displayWord = function () {
+  wordEl.innerHTML = `
+ ${selectedWord
+   .split('')
+   .map(
+     letter => `
+ <span class="letter">
+ ${correctLetters.includes(letter) ? letter : ''}
+ </span>
+ `
+   )
+   .join('')}
+ `;
+};
+
+displayWord();

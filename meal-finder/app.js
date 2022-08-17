113b145c0ca98aaabae97094df55aa83b9de34b1
Andrew Rowley <andrew@internetdrew.com>
2022-08-17 17:07:02 -0400

    Add guard clause for empty search query

--- meal-finder/app.js
+++ meal-finder/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('result-heading'),
+  single_mealEl = document.getElementById('single-meal');
+
+const clearElementsFrom = function (parentEl) {
+  while (parentEl.firstChild) {
+    parentEl.removeChild(parentEl.firstChild);
+  }
+};
+
+// Search meal and fetch from API
+const searchMeal = function (e) {
+  e.preventDefault();
+
+  // Clear single meal element
+  clearElementsFrom(single_mealEl);
+
+  // Get search term
+  const term = search.value;
+
+  // Check for empty search on submit
+  if (!term.trim()) return;
+};
+
+submit.addEventListener('submit', searchMeal);

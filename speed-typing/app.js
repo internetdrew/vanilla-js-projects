5e0a567335b9b7651425df056b34338630ba97af
Andrew Rowley <andrew@internetdrew.com>
2022-08-31 09:43:11 -0400

    Bring in DOM elements

--- speed-typing/app.js
+++ speed-typing/app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const word = document.getElementById('word'),
+  text = document.getElementById('text'),
+  scoreEl = document.getElementById('score'),
+  timeEl = document.getElementById('time'),
+  endGameEl = document.getElementById('end-game'),
+  settingsBtn = document.getElementById('settings-btn'),
+  settings = document.getElementById('settings'),
+  settingsForm = document.getElementById('settings-form'),
+  difficultySelect = document.getElementById('difficulty');

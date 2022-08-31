ae2947501d4568937302bf7b18f7e784bd37ff55
Andrew Rowley <andrew@internetdrew.com>
2022-08-31 11:53:05 -0400

    Move text focus call further down init

--- speed-typing/app.js
+++ speed-typing/app.js
@@ -52,9 +52,9 @@ const increaseScore = function () {
 const init = async function () {
   fetchWordsArr();
   await addWordsToGame();
-  text.focus();
   getRandomWord();
   addWordToDOM();
+  text.focus();
 };
 
 const handleInput = function (e) {

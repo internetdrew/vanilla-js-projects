3fc9fab4824fe2394115797f5e23eb6ffb8a3c93
Andrew Rowley <andrew@internetdrew.com>
2022-08-31 10:28:59 -0400

    Add funcs to get word and add to DOM

--- speed-typing/app.js
+++ speed-typing/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const word = document.getElementById('word'),
+const wordEl = document.getElementById('word'),
   text = document.getElementById('text'),
   scoreEl = document.getElementById('score'),
   timeEl = document.getElementById('time'),
@@ -9,3 +9,23 @@ const word = document.getElementById('word'),
   settings = document.getElementById('settings'),
   settingsForm = document.getElementById('settings-form'),
   difficultySelect = document.getElementById('difficulty');
+
+const game = {
+  score: 0,
+  time: 10,
+};
+
+const getRandomWord = async function () {
+  const res = await fetch('https://random-word-api.herokuapp.com/word');
+  if (!res.ok) throw new Error('Trouble getting response from API');
+
+  const [word] = await res.json();
+  return word;
+};
+
+const addWordToDOM = async function () {
+  const word = await getRandomWord();
+  wordEl.textContent = word;
+};
+
+addWordToDOM();

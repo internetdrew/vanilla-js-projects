7d0410bdecb6522fbc0f1045857a005ce4034fc2
Andrew Rowley <andrew@internetdrew.com>
2022-09-07 13:33:28 -0400

    Add functionality to create cards and app object

--- memory-cards/app.js
+++ memory-cards/app.js
@@ -1,1 +1,61 @@
 'use strict';
+
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
+
+const app = {
+  currentActiveCard: 0,
+  cardsData: [
+    {
+      question: 'What must a variable begin with?',
+      answer: 'A letter, $, or _',
+    },
+    {
+      question: 'What is a variable?',
+      answer: 'Container for a piece of data',
+    },
+    {
+      question: 'Example of a case sensitive variable',
+      answer: 'thisIsAVariable',
+    },
+  ],
+  cardsEl: [],
+};
+
+const createCard = function (data, index) {
+  const card = `
+      <div class="${index === 0 ? 'card active' : 'card'}">
+        <div class="inner-card">
+          <div class="inner-card-front"><p>${data.question}</p></div>
+          <div class="inner-card-back"><p>${data.answer}</p></div>
+        </div>
+      </div>
+ `;
+
+  cardsContainer.insertAdjacentHTML('beforeend', card);
+};
+
+const pushCardsToCardsEl = function () {
+  const cards = cardsContainer.querySelectorAll('.card');
+  cards.forEach(card => app.cardsEl.push(card));
+};
+
+const createCards = function () {
+  app.cardsData.forEach((data, index) => createCard(data, index));
+};
+
+const init = function () {
+  createCards();
+  pushCardsToCardsEl();
+};
+
+window.addEventListener('load', init);

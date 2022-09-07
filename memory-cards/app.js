dfe8941f10ec7286f217c040cdcafd9e703b0981
Andrew Rowley <andrew@internetdrew.com>
2022-09-07 17:37:54 -0400

    Start converting event for delegation

--- memory-cards/app.js
+++ memory-cards/app.js
@@ -11,6 +11,7 @@ const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
+const nav = document.querySelector('.navigation');
 
 const app = {
   currentActiveCard: 0,
@@ -69,3 +70,30 @@ const init = function () {
 
 window.addEventListener('load', init);
 cardsContainer.addEventListener('click', toggleShowAnswer);
+nextBtn.addEventListener('click', e => {
+  app.cardEls[app.currentActiveCard].className = 'card left';
+  app.currentActiveCard++;
+
+  if (app.currentActiveCard > app.cardEls.length - 1) {
+    app.cardEls[0].className = 'card active';
+    app.currentActiveCard = 0;
+    updateCurrentText();
+    return;
+  }
+
+  app.cardEls[app.currentActiveCard].className = 'card active';
+  updateCurrentText();
+});
+
+nav.addEventListener('click', e => {
+  const button = e.composedPath().find(el => el.nodeName === 'BUTTON');
+
+  if (!button) return;
+  console.log(button);
+
+  if (button.id === 'next') {
+  }
+
+  if (button.id === 'prev') {
+  }
+});

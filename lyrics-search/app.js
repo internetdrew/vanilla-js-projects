86a786e02f3c024a13c3d204e6d16ba0e87506d5
Andrew Rowley <andrew@internetdrew.com>
2022-09-09 08:33:29 -0400

    Add search focus on window load

--- lyrics-search/app.js
+++ lyrics-search/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+const searchSongs = async function (searchTerm) {
+  const res = await fetch(`${apiURL}/suggest/${searchTerm}`);
+  console.log(res);
+};
+
+const init = function () {
+  search.focus();
+};
+
+window.addEventListener('load', init);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) alert('Please type in a search term.');
+  console.log(searchTerm);
+  searchSongs(searchTerm);
+});

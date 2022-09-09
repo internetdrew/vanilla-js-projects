c4cdcb6d4e81b408e566964f1e5bdca237354409
Andrew Rowley <andrew@internetdrew.com>
2022-09-09 11:59:50 -0400

    Improve search results view

--- lyrics-search/app.js
+++ lyrics-search/app.js
@@ -4,8 +4,14 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const result = document.getElementById('result');
 const more = document.getElementById('more');
+const message = document.getElementById('result--message');
+const songsList = document.getElementById('songs');
 
 const apiURL = 'https://api.lyrics.ovh';
+
+const showResultsMsg = function (msg) {
+  message.textContent = msg;
+};
 
 const fetchSongsData = async function (term) {
   const res = await fetch(`${apiURL}/suggest/${term}`);
@@ -15,21 +21,41 @@ const fetchSongsData = async function (term) {
   return data;
 };
 
-const showSongsData = function (data) {
+const showSongsInfo = function (data) {
   console.log(data);
+  const searchResults = data;
+  const songsData = searchResults.data;
+
+  songsData.forEach(song => {
+    const html = `
+  <li>
+  <span><strong>${song.title}</strong> — ${song.artist.name}</span>
+  <button class="btn">Get Lyrics</button>
+  </li>
+  `;
+
+    songsList.insertAdjacentHTML('beforeend', html);
+  });
 };
 
 const init = function () {
   search.focus();
 };
 
-window.addEventListener('load', init);
-
-form.addEventListener('submit', e => {
+const handleSearch = async function (e) {
   e.preventDefault();
   const searchTerm = search.value.trim();
+  search.value = '';
 
-  if (!searchTerm) alert('Please type in a search term.');
-  const songsData = fetchSongsData(searchTerm);
-  showSongsData(songsData);
-});
+  if (!searchTerm) return;
+
+  const results = await fetchSongsData(searchTerm);
+  showResultsMsg(
+    `Showing ${results.data.length} of ${results.total} results for "${searchTerm}":`
+  );
+  showSongsInfo(results);
+};
+
+window.addEventListener('load', init);
+
+form.addEventListener('submit', handleSearch);

ab0afa9b70f2d659ff0ee48610ea712169b3f1d2
Andrew Rowley <andrew@internetdrew.com>
2022-09-09 15:05:38 -0400

    Add data attributes and pagination buttons

--- lyrics-search/app.js
+++ lyrics-search/app.js
@@ -28,30 +28,41 @@ const fetchSongsMatching = async function (term) {
 };
 
 const showSongsInfo = function (data) {
-  console.log(data);
-  const searchResults = data;
-  const songsData = searchResults.data;
+  const songsData = data.data;
 
-  songsData.forEach(song => {
-    const html = `
+  songsData
+    .map(song => {
+      const html = `
           <li>
             <span><strong>${song.title}</strong> — ${song.artist.name}</span>
-            <button class="btn">Get Lyrics</button>
+            <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Get Lyrics</button>
           </li>
   `;
 
-    songsList.insertAdjacentHTML('beforeend', html);
-  });
+      songsList.insertAdjacentHTML('beforeend', html);
+    })
+    .join('');
+
+  if (data.prev || data.next) {
+    const buttons = `
+    ${data.prev ? `<button class="btn">Prev</button>` : ''}
+    ${data.next ? `<button class="btn">Next</button>` : ''}
+`;
+
+    more.insertAdjacentHTML('beforeend', buttons);
+  }
 };
 
 const handleSearch = async function (e) {
   e.preventDefault();
+  console.log(this);
   const searchTerm = search.value.trim();
   search.value = '';
 
   if (!searchTerm) return;
 
   const results = await fetchSongsMatching(searchTerm);
+
   showResultsMsg(
     `${
       results.total === 0
@@ -59,6 +70,7 @@ const handleSearch = async function (e) {
         : `Showing ${results.data.length} of ${results.total} results for "${searchTerm}"`
     }`
   );
+
   clearElsFrom(songsList);
   showSongsInfo(results);
 };

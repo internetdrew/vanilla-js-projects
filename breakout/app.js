c40a1ea3e1dc727be056b11a69ab77a7b5870a36
Andrew Rowley <andrew@internetdrew.com>
2022-09-11 05:17:28 -0400

    Open and close rules

--- breakout/app.js
+++ breakout/app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));

7d2c1b0d24a22e1d363bbf87a5c6be7e2111b824
Andrew Rowley <andrew@internetdrew.com>
2022-09-11 04:42:29 -0400

    Add text prompt timing

--- relaxer/app.js
+++ relaxer/app.js
@@ -2,3 +2,23 @@
 
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+const breathAnimation = function () {
+  text.textContent = 'Breathe in...';
+
+  setTimeout(() => {
+    text.textContent = 'Hold';
+
+    setTimeout(() => {
+      text.textContent = 'Breathe out...';
+    }, holdTime);
+  }, breatheTime);
+};
+
+breathAnimation();
+
+setInterval(breathAnimation, totalTime);
